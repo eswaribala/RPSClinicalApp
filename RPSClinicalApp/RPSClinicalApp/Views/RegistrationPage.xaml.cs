e91365fb3650aaf8a0aceca292ff72ff7b8bdf17
eswaribala <parameswaribala@gmail.com>
2021-05-11 21:47:30 +0530

    v2

--- RPSClinicalApp/RPSClinicalApp/Views/RegistrationPage.xaml.cs
+++ RPSClinicalApp/RPSClinicalApp/Views/RegistrationPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RPSClinicalApp.Models;
+using RPSClinicalApp.Persistence;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +15,38 @@ namespace RPSClinicalApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RegistrationPage : ContentPage
     {
+        private SQLiteConnection _SQLiteConnection;
+        private int count;
         public RegistrationPage()
         {
             InitializeComponent();
+            _SQLiteConnection = DependencyService.Get<ISqliteDB>().GetSQLiteConnection();
+            _SQLiteConnection.CreateTable<User>();
+        }
+
+        private void SignUp_Clicked(object sender, EventArgs e)
+        {
+            var User = new User();
+            User.FirstName = FirstName.Text;
+            User.LastName = LastName.Text;
+            User.Password = Password.Text;
+            User.DOB = DOB.Date.ToString();
+            User.Email = Email.Text;
+            User.PhoneNo = Convert.ToInt64(Phone.Text);
+           count= _SQLiteConnection.Insert(User);
+            if (count > 0)
+            {
+                DisplayAlert("Registration", "Thanks for Registration", "Cancel");
+            }
+            else
+            {
+                DisplayAlert("Registration Failed!!!", "Please try again", "ERROR");
+            }
+        }
+
+        private void Show_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
