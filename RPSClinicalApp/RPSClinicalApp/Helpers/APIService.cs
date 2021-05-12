d7b2c7a658aa365df17f323cf621961de20b5fb6
eswaribala <parameswaribala@gmail.com>
2021-05-12 21:18:41 +0530

    v15

---
+++ RPSClinicalApp/RPSClinicalApp/Helpers/APIService.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RPSClinicalApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSClinicalApp.Helpers
+{
+    public class APIService
+    {
+        private HttpClient httpClient;
+        public ObservableCollection<MedicalCollege> MedicalColleges { get; set; }
+        public String WebAPIUrl { get; set; }
+        public APIService()
+        {
+            this.httpClient = new HttpClient();
+        }
+
+        public async Task<ObservableCollection<MedicalCollege>> RefreshAsyncData()
+        {
+            WebAPIUrl = "https://api.rootnet.in/covid19-in/hospitals/medical-colleges"; // Set your REST API url here
+            var uri = new Uri(WebAPIUrl);
+            try
+            {
+                var response = await httpClient.GetAsync(uri);
+
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Verify.....................");
+                JObject jObject = JObject.Parse(content);
+                
+                Console.WriteLine(jObject["data"]["medicalColleges"]);
+                MedicalColleges = JsonConvert.DeserializeObject<ObservableCollection<MedicalCollege>>(jObject["data"]["medicalColleges"].ToString());
+                Console.WriteLine("Verify.....................");
+                Console.WriteLine("City" + MedicalColleges[0].City);
+                return MedicalColleges;
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+    }
+}
---
+++ RPSClinicalApp/RPSClinicalApp/Models/MedicalCollege.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSClinicalApp.Models
+{
+   public class MedicalCollege
+    {
+        public String State { get; set; }
+        public String City { get; set; }
+        public String Name { get; set; }
+        public String HospitalBeds { get; set; }
+        public String Ownership { get; set; }
+        public String AdmissionCapacity { get; set; }
+    }
+}
