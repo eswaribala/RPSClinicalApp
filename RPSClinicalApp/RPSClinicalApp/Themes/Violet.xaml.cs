3bdc4c0d3c3bd851690d6812ea64b8b69a93ea29
eswaribala <parameswaribala@gmail.com>
2021-05-13 20:42:17 +0530

    v23

---
+++ RPSClinicalApp/RPSClinicalApp/Themes/Violet.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace RPSClinicalApp.Themes
+{
+	public partial class Violet : ResourceDictionary
+    {
+		public Violet()
+		{
+			InitializeComponent ();
+		}
+	}
+}
